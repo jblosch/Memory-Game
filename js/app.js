d0b2db7c14dcc135454a23097ad63338837df58e
Justin Blosch <jblosch@gmail.com>
2017-10-19 09:11:21 -0700

    Display number of moves

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 $(function() {
 
+  const baseDeck = [
+    'baseball', 'baseball',
+    'book', 'book',
+    'cocktails', 'cocktails',
+    'helmet', 'helmet',
+    'hourglass', 'hourglass',
+    'paper_plane', 'paper_plane',
+    'recycling', 'recycling',
+    'synthesizer', 'synthesizer'
+  ];
+  let compareCards = [];
+  let movesCount = 0;
+  let winCount = 0;
+
   /**
     @description Randomly assigns the card images in the grid of cards
-    @param {array} Contains a list of string names of each PNG file
+    @param {array} baseAry - A list of string names of each PNG file
+    @param {object} destination - The object to add the image string to
   */
 
   function shuffleCards(baseAry, destination) {
@@ -17,31 +32,57 @@ $(function() {
     }
   }
 
-  // Event listener for clicking on the card
-  $('.card').on('click', function(){
-    const self = $(this);
+  /**
+    @description Flips the cards horizontally; opens or closes
+    @param {object} el - The DOM element that will be flipped
+    @param {boolean} reveal - Determines whether to open the card or close it
+  */
 
+  function cardFlip(el, reveal) {
+    el.addClass('card-flip');
+    setTimeout(function(){
+      if(reveal) {
+        el.children('div').removeClass('closed');
+      } else {
+        el.children('div').addClass('closed');
+      }
+      el.removeClass('card-flip');
+    }, 200);
+  }
+
+  // Event listener for clicking on the card
+  $('.card').on('click', function() {
+    console.log();
+    const self = $(this);
     // Creates a horizontal flip animation
-    if( self.children('div').hasClass('closed') && compareCards.length < 2 ) {
-      self.toggleClass('card-flip');
-      setTimeout(function(){
-        self.children('div').removeClass('closed');
-        self.removeClass('card-flip');
-      }, 200);
+    if(self.children('div').hasClass('closed') && compareCards.length < 2) {
+      cardFlip(self, true);
+
+      // Pushes selected cards into array to compare
+      compareCards.push(self);
+
+      // Compares the cards and generates consequences
+      if(compareCards.length === 2) {
+        setTimeout(function() {
+          let first = compareCards[0][0].childNodes[1].childNodes[1].getAttribute('src');
+          let second = compareCards[1][0].childNodes[1].childNodes[1].getAttribute('src');
+          if(first === second) {
+            movesCount++;
+            $('#moves').text(movesCount);
+            winCount++;
+            compareCards = [];
+          } else {
+            movesCount++;
+            $('#moves').text(movesCount);
+            cardFlip($(compareCards[0]), false);
+            cardFlip($(compareCards[1]), false);
+            compareCards = [];
+          }
+        }, 800);
+
+      }
     }
   });
-
-  const baseDeck = [
-    'baseball', 'baseball',
-    'book', 'book',
-    'cocktails', 'cocktails',
-    'helmet', 'helmet',
-    'hourglass', 'hourglass',
-    'paper_plane', 'paper_plane',
-    'recycling', 'recycling',
-    'synthesizer', 'synthesizer'
-  ];
-  let compareCards = [];
 
   shuffleCards(baseDeck, $('.pic img'));
 
